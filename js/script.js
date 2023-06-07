68cfaacc603da8d634e86520b17227563b28b9de
NICOLAS DE SOUZA SANTOS <nicolas.ssantos15@senacsp.edu.br>
2023-06-07 17:17:51 -0300

    Feature: Adição de recursos CSS, HTML E JavaScript

--- js/script.js
+++ js/script.js
@@ -1,46 +1,45 @@
-document.getElementById("mot").onclick = ()=>{
-    document.getElementById("motor").style.border = "1px solid blue";
-    document.getElementById("motor").style.padding = "20px"
-    document.getElementById("motor").style.padding = "10px 40px 10px 40px";
-    document.getElementById("redesign").style.border = 0;
-    document.getElementById("redesign").style.padding = 0;
-    document.getElementById("otimi").style.border = 0;
-    document.getElementById("otimi").style.padding = 0;
-}   
 
+document.getElementById("redesignSites").style.display="none";
+document.getElementById("otimizacaoSites").style.display="none";
+document.getElementById("motorOtimizacao").style.display="block";
 
-document.getElementById("red").onclick = ()=> {
-    document.getElementById("redesign").style.border = "1px solid blue";
-    document.getElementById("redesign").style.padding = "10px 40px 10px 40px";
-    document.getElementById("motor").style.border = 0;
-    document.getElementById("motor").style.padding = 0;
-    document.getElementById("otimi").style.border = 0;
-    document.getElementById("otimi").style.padding = 0;  
-}
-
-document.getElementById("oti").onclick = ()=> {
-    document.getElementById("otimi").style.border = "1px solid blue";
-    document.getElementById("otimi").style.padding = "20px";
-    document.getElementById("otimi").style.padding = "10px 40px 10px 40px";
-    document.getElementById("redesign").style.border = 0;
-    document.getElementById("redesign").style.padding = 0;
-    document.getElementById("motor").style.border = 0;
-    document.getElementById("motor").style.padding = 0;
-}
 
 
 document.getElementById("mot").addEventListener("mouseover", function( event ) {
-
-    document.getElementById("motor").style.border = "1px solid blue";
-    document.getElementById("motor").style.padding = "10px 40px 10px 40px";
+   
     document.getElementById("redesign").style.border = "none";
     document.getElementById("redesign").style.padding = "none";
+    document.getElementById("otimi").style.padding = "none";
+    document.getElementById("otimi").style.border = "none";
+    document.getElementById("redesignSites").style.display="none";
+    document.getElementById("otimizacaoSites").style.display="none";
+    document.getElementById("motorOtimizacao").style.display="block";
+    
+});
 
-})
+
 
 document.getElementById("red").addEventListener("mouseover", function( event ) {
-    document.getElementById("redesign").style.border = "1px solid blue";
-    document.getElementById("redesign").style.padding = "10px 40px 10px 40px";
+   
     document.getElementById("motor").style.border = "none";
     document.getElementById("motor").style.padding = "none";
+    document.getElementById("otimi").style.padding = "none";
+    document.getElementById("otimi").style.border = "none";
+    document.getElementById("motorOtimizacao").style.display="none";
+    document.getElementById("otimizacaoSites").style.display="none";
+    document.getElementById("redesignSites").style.display="block";
+
 });
+
+document.getElementById("oti").addEventListener("mouseover", function( event ) {
+
+    
+    document.getElementById("redesign").style.border = "none";
+    document.getElementById("redesign").style.padding = "none";
+    document.getElementById("motor").style.padding = "none";
+    document.getElementById("motor").style.border = "none";
+    document.getElementById("motorOtimizacao").style.display="none";
+    document.getElementById("redesignSites").style.display="none";
+    document.getElementById("otimizacaoSites").style.display="block";
+})
+
